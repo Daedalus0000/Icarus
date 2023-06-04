4274e5e30af32b085fae390bf6a38190ffdeb0e8
Daedalus <135420849+Daedalus0000@users.noreply.github.com>
2023-06-04 10:53:45 +0200

    Update icarus.sol

--- contracts/icarus.sol
+++ contracts/icarus.sol
@@ -285,8 +285,11 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     * - 'sender' must have a balance of at least 'amount'.
     * - the caller must have allowance for 'sender' tokens of at least 'amount'.
     */
-    function transferFrom(address sender, address recipient, uint256 amount)
-    public virtual override returns (bool) {
+     function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
         _transfer(sender, recipient, amount);
         uint256 currentAllowance = _allowances[sender][_msgSender()];
         require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
@@ -295,6 +298,175 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         }
         return true;
     }
+    
+    /*
+    * @dev Atomically increases the allowance granted to 'spender' by the caller.
+    * This is an alternative to {approve} that can be used as a mitigation for
+    * problems described in {IERC20-approve}.
+    * Emits an {Approval} event indicating the updated allowance.
+    * Requirements: - 'spender' cannot be the zero address.
+    */
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
+    }
+    
+    /*
+    * @dev Atomically decreases the allowance granted to 'spender' by the caller.
+    * This is an alternative to {approve} that can be used as a mitigation for
+    * problems described in {IERC20-approve}.
+    * Emits an {Approval} event indicating the updated allowance.
+    * Requirements:
+    * - 'spender' cannot be the zero address.
+    * - 'spender' must have allowance for the caller of at least 'subtractedValue'.
+    */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = allowance(_msgSender(), spender);
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
+        return true;
+    }
+    
+    /*
+    * @dev Moves 'amount' of tokens from 'sender' to 'recipient'.
+    * This internal function is equivalent to {transfer}, and can be used to
+    * e.g. implement automatic token fees, slashing mechanisms, etc.
+    * Emits a {Transfer} event.
+    * Requirements:
+    * - 'sender' cannot be the zero address.
+    * - 'recipient' cannot be the zero address.
+    * - 'sender' must have a balance of at least 'amount'.
+    */
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        _beforeTokenTransfer(sender, recipient, amount);
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[sender] = senderBalance - amount;
+            /*
+            * Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is
+            * preserved by decrementing then incrementing.
+            */
+            _balances[recipient] += amount;
+        }
+        emit Transfer(sender, recipient, amount);
+        _afterTokenTransfer(sender, recipient, amount);
+    }
+    
+    /* @dev Creates 'amount' tokens and assigns them to 'account', increasing the total supply.
+    * Emits a {Transfer} event with `from` set to the zero address.
+    * Requirements: - 'account' cannot be the zero address.
+    */
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _beforeTokenTransfer(address(0), account, amount);
+        _totalSupply += amount;
+        unchecked {
+            // Overflow not possible: balance + amount is at most totalSupply + amount, which is checked above.
+            _balances[account] += amount;
+        }
+        emit Transfer(address(0), account, amount);
+        _afterTokenTransfer(address(0), account, amount);
+    }
+    
+    /*
+    * @dev Destroys 'amount' tokens from 'account', reducing the total supply.
+    * Emits a {Transfer} event with 'to' set to the zero address.
+    * Requirements:
+    * - 'account' cannot be the zero address.
+    * - 'account' must have at least `amount` tokens.
+    */
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+        _beforeTokenTransfer(account, address(0), amount);
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+            // Overflow not possible: amount <= accountBalance <= totalSupply.
+            _totalSupply -= amount;
+        }
+        emit Transfer(account, address(0), amount);
+        _afterTokenTransfer(account, address(0), amount);
+    }
+    
+    /*
+    * @dev Sets 'amount' as the allowance of 'spender' over the 'owner's tokens.
+    * This internal function is equivalent to 'approve', and can be used to
+    * e.g. set automatic allowances for certain subsystems, etc.
+    * Emits an {Approval} event.
+    * Requirements:
+    * - 'owner' cannot be the zero address.
+    * - 'spender' cannot be the zero address.
+    */
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+    
+    /*
+    * @dev Updates 'owner's allowance for 'spender' based on spent 'amount'.
+    * Does not update the allowance amount in case of infinite allowance.
+    * Revert if not enough allowance is available.
+    * Might emit an {Approval} event.
+    */
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+            unchecked {
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
+    
+    /*
+    * @dev Hook that is called before any transfer of tokens. This includes minting and burning.
+    * Calling conditions:
+    * - when 'from' and 'to' are both non-zero, 'amount' of 'from's tokens will be transferred to 'to'.
+    * - when 'from' is zero, 'amount' tokens will be minted for 'to'.
+    * - when 'to' is zero, 'amount' of 'from's tokens will be burned.
+    * - 'from' and 'to' are never both zero.
+    */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+
+    /*
+    * @dev Hook that is called after any transfer of tokens. This includes minting and burning.
+    * Calling conditions:
+    * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens has been transferred to `to`.
+    * - when `from` is zero, `amount` tokens have been minted for `to`.
+    * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
+    * - `from` and `to` are never both zero.
+    */
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+}
     
     
 
